ef0ab85864c8f13924aea91419961d76245de39f
sarahazzabi <55262363+sarahazzabi@users.noreply.github.com>
2019-11-17 07:49:52 -0500

    using the created product.service to connect to backend and get all products from database

--- Frontend/src/app/product.service.ts
+++ Frontend/src/app/product.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-
-  constructor() { }
+  uri = 'http://localhost:8081';
+  constructor(private http: HttpClient) { }
+  fetchProducts() {
+    return this.http.get(`${this.uri}/products`);
+  }
 }
